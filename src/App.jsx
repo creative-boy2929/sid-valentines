cadef72e6c03f1749aaa81d1cac8c246c2ee727f
Vaibhav-Tonde <vaibhavtonde2929@gmail.com>
2026-02-14 00:59:47 +0530

    Fix router for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HeroSlide from "./components/HeroSlide";
 import OptionsSlide from "./components/OptionsSlide";
 import ProposalSlide from "./components/ProposalSlide";
@@ -6,27 +6,17 @@ import Letter from "./components/Letter";
 import Gallery from "./components/Gallery";
 import Quiz from "./components/Quiz";
 
-// function Letter() {
-//   return <div className="h-screen bg-black text-white flex items-center justify-center text-5xl">Letter Page 💌</div>;
-// }
-
-// function Gallery() {
-//   return <div className="h-screen bg-black text-white flex items-center justify-center text-5xl">Gallery Page 🖼</div>;
-// }
-
-// function Quiz() {
-//   return <div className="h-screen bg-black text-white flex items-center justify-center text-5xl">Quiz Page 🎯</div>;
-// }
-
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<>
-          <HeroSlide />
-          <OptionsSlide />
-          <ProposalSlide/>
-        </>} />
+        <Route path="/" element={
+          <>
+            <HeroSlide />
+            <OptionsSlide />
+            <ProposalSlide />
+          </>
+        } />
         <Route path="/letter" element={<Letter />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/quiz" element={<Quiz />} />
